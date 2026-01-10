ca716034645be3baf3b2e41ea8835fd54f02b0df
PhatTB1004 <phatdaothien@gmail.com>
2026-01-10 17:51:44 +0700

    Tuan01/TTNT_01/Bai07 - Bai lam

---
+++ Tuan01/Lession1/ex3_circle.py
@@ -0,0 +1,6 @@
+print("Calculate the circumference and area of ​​a circle.")
+r = float(input("Moi ban nhap ban kinh r: "))
+cv = 2*3.14*r
+dt = 3.14*r*r
+print("Chu vi hinh tron la: %.2f" % cv)
+print("Dien tich hinh tron la: %.2f" % dt)
